99682c2060ddb41358aee40a257a34e0df3a9407
Niranjan Padhy <niranjanpadhi@gmail.com>
2024-06-04 18:16:20 +0530

    grouping

---
+++ grouping.js
@@ -0,0 +1,8 @@
+var cars = [{ make: 'audi', model: 'r8', year: '2012' }, { make: 'audi', model: 'rs5', year: '2013' }, { make: 'ford', model: 'mustang', year: '2012' }, { make: 'ford', model: 'fusion', year: '2015' }, { make: 'kia', model: 'optima', year: '2012' }],
+    result = cars.reduce(function (r, a) {
+        r[a.make] = r[a.make] || [];
+        r[a.make].push(a);
+        return r;
+    }, Object.create(null));
+
+console.log(result);
